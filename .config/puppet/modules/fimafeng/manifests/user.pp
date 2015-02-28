27018c03d70682240483c684ad5bc44c039b1bdf
Matt Glaman <nmd.matt@gmail.com>
2015-02-27 18:29:43 -0600

    Add dotfiles for provisioned users (root/aegir.)

--- .config/puppet/modules/fimafeng/manifests/user.pp
+++ .config/puppet/modules/fimafeng/manifests/user.pp
@@ -18,4 +18,19 @@ define fimafeng::user (
       ensure => directory,
     }
   }
+
+  file {
+    "${home}/.profile":
+    source => ["/vagrant/.config/files/.profile",
+      "puppet:///modules/fimafeng/profile"];
+    "${home}/.bashrc":
+      source => ["/vagrant/.config/files/.bashrc",
+        "puppet:///modules/fimafeng/bashrc"];
+    "${home}/.bash_aliases":
+      source => ["/vagrant/.config/files/.bash_aliases",
+        "puppet:///modules/fimafeng/bash_aliases"];
+    "${home}/.vimrc":
+      source => ["/vagrant/.config/files/.vimrc",
+        "puppet:///modules/fimafeng/vimrc"];
+  }
 }
--- README.md
+++ README.md
@@ -29,3 +29,12 @@ address=/aegir.dev/10.22.22.0
 ## Creating environments
 
 @todo: Create some fancy scripts to help setup Panopoly, Commerce Kickstart, etc.
+
+# Credit/Thanks
+
+I borrowed/copied items from all of the below awesome open source projects which made this possible and taught me a little bit about Puppet.
+
+* [Puppet Aegir](https://www.drupal.org/project/puppet_aegir)
+* [Puppet Drush](https://www.drupal.org/project/puppet_drush)
+* [Drush Vagrant](https://www.drupal.org/project/drush-vagrant)
+* [PuPHPet](https://puphpet.com/)
