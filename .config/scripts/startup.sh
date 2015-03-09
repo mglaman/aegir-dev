3193d3e98c2c6e27d2f6809dac6d8bc936a77e2e
Matt Glaman <nmd.matt@gmail.com>
2015-03-08 19:51:02 -0500

    Setup ability to allow easier development of contrib Aegir work

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 # Need this because Apache is running on boot, methinks.
 sudo apachectl graceful
+
+# Setup symlink for Aegir module development...
+if [ -d "/vagrant/aegir-dev/modules" ]; then
+	ln -s /vagrant/aegir-dev/modules /var/aegir/hostmaster/sites/all/modules/dev
+fi
+
+# Setup symlink for Aegir theme development...
+if [ -d "/vagrant/aegir-dev/themes" ]; then
+	ln -s /vargrant/aegir-dev/themes /var/aegir/hostmaster/sites/all/themes/dev
+fi
+
+if [ -d "/vargrant/aegir-dev/drush" ]; then
+	ln -s /vagrant/aegir-dev/drush /var/aegir/.drush/dev
+fi
---
+++ aegir-dev/README.md
@@ -0,0 +1,10 @@
+This folder provides a method for writing Aegir tools.
+
+## Modules
+Create a "modules" folder. All folders will become symlinked to Hostmaster's sites/all/modules/dev
+
+## Themes
+Create a "themes" folder. Symlinked to Hostmaster's sites/all/themes/dev
+
+## Drush/Provision
+Create a "drush" folder. Smylinks to /var/aegir/.drush/dev
